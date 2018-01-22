9c985a5a8be7273dcccae7ec89e7f30fecb2e0b2
Jimmy Sanders-Cannon <jimmysancan@yahoo.com>
2018-01-22 17:55:16 -0500

    server

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ function sortString(string) {
   if (typeof string !== 'string') {
     return alert('please input a string')
   }
+  addToSortedList(string.split('').sort().join(''))
   return string.split('').sort().join('')
 }
 
+const sortedList = [];
+
+function addToSortedList(string) {
+  sortedList.unshift(string)
+}
+
+function displayList(array) {
+  array.forEach((word) => {
+    console.log(word)
+  })
+}
 
 
